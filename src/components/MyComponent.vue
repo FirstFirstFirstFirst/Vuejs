4fc678198cc4e623fa606e8aa79fb398fc333f07
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2023-10-16 11:31:26 +0700

    add student and many components

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h6>This is my component</h6>
+  <SecondComponent></SecondComponent>
+</template>
+<script>
+import { defineComponent } from "vue";
+import SecondComponent from "./SecondComponent.vue";
+export default defineComponent({
+  name: "MyComponent",
+  components: { SecondComponent },
+});
+</script>
--- src/pages/MainComponentPage.vue
+++ src/pages/MainComponentPage.vue
@@ -1,12 +1,14 @@
 <template>
   <q-page padding>
-    <h6>This is MainComponentPage</h6>
+    <h6>This is main component page</h6>
+    <MyComponent></MyComponent>
   </q-page>
 </template>
 <script>
 import { defineComponent } from "vue";
-
+import MyComponent from "../components/MyComponent.vue";
 export default defineComponent({
   name: "MainComponentPage",
+  components: { MyComponent },
 });
 </script>
